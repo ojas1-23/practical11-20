544023fe69d9a0feef02efd39168c84dfd0f993f
ojasvi shukla <ojasvishukla16@gmail.com>
2025-09-04 20:19:08 +0530

    Update p27.php

--- p27.php
+++ p27.php
@@ -1,1 +1,8 @@
-
+<?php
+$arr=[1,2,3,4,5,6];
+echo"Reverse(without using array reverse=";
+for($i=count($arr)-1;$i>=0;$i--)
+echo $arr[$i]." ";
+echo "<br>Reverse(function): ";
+print_r(array_reverse($arr));
+?>
