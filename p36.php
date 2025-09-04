c512d9869a91acef2f5fce2c29d6a3478bdc0754
ojasvi shukla <ojasvishukla16@gmail.com>
2025-09-05 00:23:02 +0530

    Update p36.php

--- p36.php
+++ p36.php
@@ -2,3 +2,4 @@
 $name = $_GET['name'];
 echo "Hello, $name";
 ?>
+//http://localhost/php2025practicals/practical%2011-20/practical11-20/p36.php?name=Rahul---to run this write in URL  after '?' question mark name=Rahul
