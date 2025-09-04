a3402c0e45fc9d3fef1a6d685552132ac2960df1
ojasvi shukla <ojasvishukla16@gmail.com>
2025-09-05 00:10:04 +0530

    Update p34.php

--- p34.php
+++ p34.php
@@ -1,1 +1,12 @@
+<?php
+$fruits = ["Banana" => 30, "Apple" => 50, "Mango" => 40];
+
+echo "Sorted by values:<br>";
+asort($fruits);
+print_r($fruits);
+
+echo "<br><br>Sorted by keys:<br>";
+ksort($fruits);
+print_r($fruits);
+?>
 
