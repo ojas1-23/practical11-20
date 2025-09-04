e04882f4f4501e443820c8e1ebd406a0e1b81ec7
ojasvi shukla <ojasvishukla16@gmail.com>
2025-09-05 00:27:15 +0530

    Update p37.php

--- p37.php
+++ p37.php
@@ -1,1 +1,6 @@
-
+<?php
+$name = $_GET['name'];
+$age = $_GET['age'];
+echo "Hello $name, you are $age years old.";
+?>
+//http://localhost/php2025practicals/practical%2011-20/practical11-20/p37.php?name=anita&age=22
