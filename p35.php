86531686c819af2bfded7c2c54e118c95b5340de
ojasvi shukla <ojasvishukla16@gmail.com>
2025-09-05 00:10:49 +0530

    Update p35.php

--- p35.php
+++ p35.php
@@ -1,1 +1,12 @@
+<?php
+$countries = ["India" => "New Delhi", "USA" => "Washington", "Germany" => "Berlin"];
+$search = "Berlin";
+$country = array_search($search, $countries);
+
+if ($country !== false) {
+    echo "$search is the capital of $country.";
+} else {
+    echo "Capital not found.";
+}
+?>
 
