02cb259b83ac248d575fcb767e278638d6c48a3a
ojasvi shukla <ojasvishukla16@gmail.com>
2025-09-04 20:10:07 +0530

    Update p26.php

--- p26.php
+++ p26.php
@@ -1,4 +1,8 @@
 <?php
-$arr=[5,1,9,3,7];
-echo"Largest=".max($arr)."Largest=".min($arr);
+$arr=[1,2,3,4,5,6];
+echo"Reverse(without using array reverse=";
+for($i=count($arr)-1;$i>=0;$i--)
+echo $arr[$i]." ";
+echo "<br>Reverse(function): ";
+print_r(array_reverse($arr));
 ?>
